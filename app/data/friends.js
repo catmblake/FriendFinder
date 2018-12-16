60c55d075c8bed1a0e944fe4581c5712d8d44868
catmblake <catmblake@gmail.com>
2018-12-15 17:51:01 -0800

    added friends array to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+// array of potential friends objects
+var friends = [
+    {
+        name: "Amy",
+        photo: "https://www.pexels.com/photo/portrait-photo-of-smiling-woman-wearing-white-sweater-1171032/",
+        scores: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+    }
+];
