1ce75d5d7f5a466f586c9a3d7a750e73a60b4802
catmblake <catmblake@gmail.com>
2018-12-17 14:38:36 -0800

    reordered code in server.js to fix bugs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,15 @@
 var userData = require("../data/friends.js");
 
-module.exports = function(app) {
-app.get("/api/survey", function(req, res){
-    res.json(userData);
-})
+module.exports = function (app) {
+    app.get("/api/survey", function (req, res) {
+        res.json(userData);
+    });
+    app.post("/api/friends", function (req, res) {
+        var user = req.body;
+        userData.push(user);
+        res.send(userData);
+    });
+    app.get("/api/friends", function (req, res) {
+        res.json(userData);
+    });
 };
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 // requiring dependencies
 var express = require("express");
 var app = express();
-var path = require("path");
 var PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
-app.listen(PORT, function() {
-console.log(`App is listening on port ${PORT}`);
+
+app.listen(PORT, function () {
+    console.log(`App is listening on port ${PORT}`);
 });
