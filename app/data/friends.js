9dd2fcb112cff5e5cd589b3a1dd2f101da196b63
catmblake <catmblake@gmail.com>
2018-12-18 20:49:13 -0800

    changed some variable names to be more descriptive

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,50 +1,50 @@
 // array of potential friends objects
-var userData = [
+var friends = [
     {
         "name": "Amy",
         "photo": "https://images.pexels.com/photos/1139793/pexels-photo-1139793.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500",
         "scores": [
-            5, 
-            5, 
-            5, 
-            5, 
-            5, 
-            5, 
-            5, 
-            5, 
-            5, 
-            5]
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5", 
+            "5"]
     },
     {
         "name": "Jill",
         "photo": "https://images.pexels.com/photos/713312/pexels-photo-713312.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500",
         "scores": [
-            3, 
-            3, 
-            3, 
-            3, 
-            3, 
-            3, 
-            3, 
-            3, 
-            3, 
-            3]
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3", 
+            "3"]
     },
     {
         "name": "Ray",
         "photo": "https://images.pexels.com/photos/901236/pexels-photo-901236.jpeg?auto=compress&cs=tinysrgb&dpr=1&fit=crop&h=500&w=500",
         "scores": [
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1]
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1", 
+            "1"]
     }
 ];
-// exporting the array userData
-module.exports = userData;
+// exporting the array friends
+module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,24 @@
-// requiring the userData from friends.js
-var userData = require("../data/friends.js");
+// requiring the friends from friends.js
+var friends = require("../data/friends.js");
 
 // exporting the following function to be used in server.js
 module.exports = function (app) {
     // setting a get request and response for api/friends
     app.get("/api/friends", function (req, res) {
-        res.json(userData);
+        res.json(friends);
     }); //setting a post request and response for api/friends
     app.post("/api/friends", function (req, res) {
-        // creating newUser variable which holds the user object. newUserScores stores answers in an array 
-        var newUser = req.body;
-        var newUserScores = req.body.scores;
+        // creating user variable which holds the user object. newUserScores stores answers in an array 
+        var user = req.body;
+        var userScores = req.body.scores;
         // empty array for calculations comparing user's answers with potential friends
         var totalDifference = [];
         // looping through the array of friends and setting scoresDiff to 0 
-        for (var i = 0; i < userData.length; i++) {
+        for (var i = 0; i < friends.length; i++) {
             var scoresDiff = 0
-            // nested loop that calculates difference between newUser scores and each potential friend
-            for (var j = 0; j < newUserScores.length; j++) { 
-                scoresDiff += Math.abs(userData[i].scores[j] - parseInt(newUserScores[j]));
+            // nested loop that calculates difference between user scores and each potential friend
+            for (var j = 0; j < userScores.length; j++) { 
+                scoresDiff += Math.abs(parseInt(friends[i].scores[j]) - parseInt(userScores[j]));
             };
             // adding the scoresDiff to the totalDifference array
             totalDifference.push(scoresDiff);
@@ -34,10 +34,10 @@ module.exports = function (app) {
             }
         }
         // declaring bestFriend variable  
-        var bestFriend = userData[bestMatch];
+        var bestFriend = friends[bestMatch];
         // sending the bestFriend JSON in response to the post request
         res.json(bestFriend);
-        // adding the newUser to the userData array of potential friends 
-        userData.push(newUser);
+        // adding the user to the friends array of potential friends 
+        friends.push(user);
     });
 };
